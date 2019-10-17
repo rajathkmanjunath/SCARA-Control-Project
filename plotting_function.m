412786582008d8616f328a97e31e1322735663ee
rajathkmanjunath <rajath.km@nyu.edu>
2019-10-16 20:41:45 -0400

    changing the nonlinear function

--- plotting_function.m
+++ plotting_function.m
@@ -1,6 +1,7 @@
 % Plotting function
 figure(1)
 state_names = ["theta_1", "theta_2", "d_3", "theta_4"];
+title("q");
 for i=1:4
 subplot(4,1,i);
 plot(t, out.q.Data(:,i));
@@ -9,6 +10,7 @@ ylabel(state_names(i));
 end
 
 figure(2)
+title("p");
 definition_names = ["x","y","z","theta_x","theta_y","theta_z"];
 for i=1:6
 subplot(6,1,i);
@@ -18,6 +20,7 @@ ylabel(definition_names(i));
 end
 
 figure(3)
+title("error");
 definition_names = ["x","y","z","theta_x","theta_y","theta_z"];
 for i=1:6
 subplot(6,1,i);
@@ -25,3 +28,12 @@ plot(t, out.error.Data(:,i));
 xlabel("time");
 ylabel(definition_names(i));
 end
+
+figure(4)
+for i=1:3
+    subplot(4,1,i);
+    plot(t, pd(:,i));
+    
+end
+subplot(4,1,4);
+plot(t, theta_d(:));
