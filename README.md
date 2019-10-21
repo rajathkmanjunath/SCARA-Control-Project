06b97d5dea2d00b113cf7fae543e634fe56caf72
rajathkmanjunath <rajath.km@nyu.edu>
2019-10-21 19:59:26 -0400

    adding all the files of the project

--- README.md
+++
@@ -1,2 +0,0 @@
-# SCARA-Control-Project
-Sensor based Robotics Project Fall-19
---
+++ question-2/Part-a (jacobian_end_constraint)/init.m
@@ -0,0 +1,39 @@
+clear all
+close all
+clc
+
+load('kinematic_traj.mat');
+
+sim('jacobian_pseudo_inverse_2018b',4); % Simulating the jacobian inverse function
+scatter3(ans.xe.Data(:,1),ans.xe.Data(:,2),ans.xe.Data(:,3))%Plotting the function in 3 D
+xlabel('x');
+ylabel('y');
+zlabel('z');
+
+figure('Name','joint variables plots');
+labeljoint = ["theta_1","theta_2","d_3","theta_4"];
+for i=1:4
+    subplot(4,1,i);
+    plot(t,ans.q.Data(:,i));
+    xlabel('time');
+    ylabel(labeljoint(i));
+end 
+
+figure('Name','error plots');
+labelerror = ["e_x","e_y","e_z"];
+for i=1:3
+    subplot(3,1,i);
+    plot(t,ans.error.Data(:,i));
+    xlabel('time');
+    ylabel(labelerror(i));
+end 
+
+figure('Name','operational variables plots');
+labeloperation = ["x","y","z"];
+for i=1:3
+    subplot(3,1,i);
+    plot(t,ans.xe.Data(:,i));
+    xlabel('time');
+    ylabel(labeloperation(i));
+end 
+
